9678e790c6a4daf4fc65368aed0257efd06340b4
gaju11 <gajumurkute11@gmail.com>
2026-07-10 11:23:27 +0530

    change value again

--- src/main/java/com/casaone/converge/Test.java
+++ src/main/java/com/casaone/converge/Test.java
@@ -1,7 +1,7 @@
 package com.casaone.converge;
 
 public class Test {
-    int x=50;
+    int x=90;
     int y=20;
 
     public int add(int x,int y)
@@ -11,7 +11,7 @@ public class Test {
     public static void main(String[] args) {
 
         Test t=new Test();
-       int sum= t.add(50,20);
+       int sum= t.add(90,20);
         System.out.println(sum);
     }
 }
